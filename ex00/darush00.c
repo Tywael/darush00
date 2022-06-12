990a52e2c3e9f465f1f11594841849d7048f362c
yalthaus <yves.althaus42@gmail.com>
2022-06-12 16:18:08 +0200

    voilas

--- ex00/darush00.c
+++ ex00/darush00.c
@@ -1,26 +1,12 @@
-// o = 111
-// - = 45
-// " " = 32
-// | = 124
 #include <unistd.h>
-
-void rush(int x, int y)
-{
-    //int i = x;
-    (void)y;
-    int n=y;
-    char *s = "-o |";
-    while(n-->0)
-    {
-        for(int i=x;i>0;i--)
-            if(n==0||n==y-1)write(1, &s[(i==0)+(i==x-1)], 1);
-            else write(1, &s[(i==0)+(i==x-1)+2], 1);
-        write(1,"\n",1);
-    }
-}
+void rush(int x,int y){int n=y;char *s="-o |";while(n-->0){for(int i=x;i>0;i--)if(n==0||n==y-1)write(1,&s[(i==1)||(i==x)],1);else write(1,&s[(i==1)+(i==x)+2],1);write(1,"\n",1);}}
 
 int main ()
 {
+    rush(0, 0);
+    rush(1, 1);
+    rush(2, 2);
+    rush(3, 3);
     rush(4, 4);
     return (0);
 }
